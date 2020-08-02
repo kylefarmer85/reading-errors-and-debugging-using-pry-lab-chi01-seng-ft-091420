22509b309997ea6eed4129759f667613b6d304e2
Kyle Farmer <kylefarmer85@gmail.com>
2020-08-02 22:34:16 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,13 +12,10 @@ end
 
 def selection(num)
   if num == 1
-    binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
-  binding.pry
-  puts "hello"
 end
 
 def runner

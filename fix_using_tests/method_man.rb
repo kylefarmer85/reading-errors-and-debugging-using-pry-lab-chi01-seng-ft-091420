05847863723769e40f424f7f800f40f98cabb1a1
Kyle Farmer <kylefarmer85@gmail.com>
2020-08-02 22:39:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 def start_game(player1, player2)
+    binding.pry
   p "Hello #{player1} & #{player2}"
 end
 
 def play_game(player2)
   p "#{player1} is better than #{player2}"
-  binding.pry
-  puts "test"
+
 end
